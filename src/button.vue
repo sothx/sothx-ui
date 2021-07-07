1f9a27fbdeb0f9da5de2a595da998eda3619be31
sothx <i@sothx.com>
2021-07-08 00:17:03 +0800

    新增parcel

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="s-button">hi</button>
+</template>
+
+<script>
+  export default {
+    name: "",
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .s-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
